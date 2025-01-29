369cd816c617a930d7d3b7f777f0366cac8ae145
Gláufer Charão <glaufer.charao@outlook.com>
2025-01-29 10:55:57 -0300

    Create project.

---
+++ src/main/java/com/gsam/technology/todostefanini/TodoStefaniniApplication.java
@@ -0,0 +1,13 @@
+package com.gsam.technology.todostefanini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoStefaniniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoStefaniniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsam/technology/todostefanini/TodoStefaniniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsam.technology.todostefanini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoStefaniniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
